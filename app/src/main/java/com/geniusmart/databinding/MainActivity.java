5fb3c6b1b509cf0cf2dba850681638f7698f20a8
geniusmart <270391484@qq.com>
2016-04-08 18:32:30 +0800

    commint

--- app/src/main/java/com/geniusmart/databinding/MainActivity.java
+++ app/src/main/java/com/geniusmart/databinding/MainActivity.java
@@ -4,12 +4,14 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.geniusmart.databinding.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MainActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
     }
 }
